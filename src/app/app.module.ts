2f3688631081e589480474179f7a9cb476c1a711
Derrick Fink <DerrickFink@boisestate.edu>
2016-11-25 20:45:39 -0700

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { AboutComponent } from './components/about/about.component';
+
+import { Routes } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(Routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { SearchComponent } from './components/search/search.component'
+import { AboutComponent } from './components/about/about.component'
+
+export const Routes = [
+    { path: '', component: SearchComponent },
+    { path: 'about', component: AboutComponent }
+];
